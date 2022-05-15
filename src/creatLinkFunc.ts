d53c3e5ac70e0e91b2822f8ce515a1007dcb846c
lqq <623903979@qq.com>
2022-05-15 18:36:53 +0800

    reverse listNodes

--- src/creatLinkFunc.ts
+++ src/creatLinkFunc.ts
@@ -1,13 +1,13 @@
-/**
- * @description 把数组转换成链表的函数
- */
-
 interface listNode {
   value: number;
   next?: listNode;
 }
 
-function createLinks(arr: number[]): listNode {
+/**
+ * @description 把数组转换成链表的函数
+ */
+
+export function createLinks(arr: number[]): listNode {
   const length = arr.length;
   if (!length) throw new Error("arr length is must be > 0");
 
@@ -26,4 +26,34 @@ function createLinks(arr: number[]): listNode {
   }
 
   return curNode;
+}
+
+/**
+ * @description  反转单向链表
+ */
+
+export function reverseListNode(listNode: listNode): listNode {
+  // 创建三个指针
+  let currentNode: listNode | undefined = undefined;
+  let previewNode: listNode | undefined = undefined;
+  let nextNode: listNode | undefined = listNode;
+  while (nextNode) {
+    // 删除第一个节点的next 防止死循环
+    if (currentNode && !previewNode) {
+      delete currentNode.next;
+    }
+    // 反转节点
+    if (currentNode && previewNode) {
+      currentNode.next = previewNode;
+    }
+
+    // 将三个指针向后瞬移
+    previewNode = currentNode;
+    currentNode = nextNode;
+    nextNode = nextNode.next;
+  }
+
+  // 处理 最后一个没有next的节点
+  currentNode!.next = previewNode;
+  return currentNode!;
 }
